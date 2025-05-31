3b0bfa3d56b79168db4b1c05e552df1b16038c99
Maksymilian Tulewicz <maksymilian.tulewicz@gmail.com>
2025-05-31 14:04:47 +0000

    Struktura poprawiona możemy zaczynać

--- src/tests_node.cpp
+++ src/tests_node.cpp
@@ -1,44 +1,22 @@
-#include "hexapod_project/one_leg_gait.h"
 #include <ros/ros.h>
+#include <std_msgs/Float64.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "hexapod_tests_node");
     ros::NodeHandle nh;
 
-    // Test One Leg Gait
+    ROS_INFO("Hexapod Tests Node started");
+
+    // Na razie prosty node który tylko się uruchamia
+    // Tutaj będziemy dodawać kolejne testy gaitów
+
+    ros::Rate rate(10); // 10 Hz
+    while (ros::ok())
     {
-        ROS_INFO("Testing One Leg Gait");
-        hexapod::OneLegGait gait(nh);
-        gait.initialize();
-
-        // Set custom parameters if needed
-        gait.setStepSize(4.5, 2.5);
-        gait.setSpeed(2.0);
-
-        // Execute the gait
-        gait.execute();
+        ros::spinOnce();
+        rate.sleep();
     }
-
-    // Here you can add tests for other gaits
-    // Just uncomment the section you want to test
-    /*
-    {
-        ROS_INFO("Testing Two Legs Gait");
-        hexapod::TwoLegsGait gait(nh);
-        gait.initialize();
-        gait.execute();
-    }
-    */
-
-    /*
-    {
-        ROS_INFO("Testing Three Legs Gait");
-        hexapod::ThreeLegsGait gait(nh);
-        gait.initialize();
-        gait.execute();
-    }
-    */
 
     return 0;
 }
